0a843e9cedd3034a4cbe951bf5bde4e22bcaed6e
Caleb E <mkcnj01@gmail.com>
2024-05-24 07:18:04 -0500

    Random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,13 @@ var enemyHealth = 50;
 
 var enemyAttack = 12;
 
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
+
 var fight = function(enemyName) {
     //Repeat and execute as long as the enemy-robot is alive
     while(playerHealth > 0 && enemyHealth > 0) {
@@ -30,14 +37,16 @@ var fight = function(enemyName) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
 
                 //Subtract Money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
         
-        //Remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        //Generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack -3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         
         //Log a resulting message to the console so we know that it worked
         console.log(
@@ -58,8 +67,10 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
-        //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and usw that result to update the value of the 'playerHealth' variable
-        playerHealth = playerHealth - enemyAttack;
+        //Generate random damage based on enemy's attack power
+        var damage = randomNumber(enemyAttack -3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
 
         //Log a resulting message to the console so we know that it worked
         console.log(
@@ -92,7 +103,7 @@ var startGame = function() {
             
             var pickedEnemyName= enemyNames[i];
             
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             
             //call fight function with enemy-robot
             fight(pickedEnemyName);
